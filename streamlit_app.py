89e49c3d4de9e8b5217d6f437c570f00e19d4152
Savannah Carnahan <87336772+savannahcarnahan@users.noreply.github.com>
2022-08-15 16:49:16 -0400

    Update streamlit_app.py
    Control of information issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,5 @@ streamlit.dataframe(my_data_rows)
 
 fruit_choice2 = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thank you for adding ', fruit_choice2)
+
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
