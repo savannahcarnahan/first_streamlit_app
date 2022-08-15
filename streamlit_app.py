cced77fc277d6ce5bb61a59b950c3ff01e306f1a
Savannah Carnahan <87336772+savannahcarnahan@users.noreply.github.com>
2022-08-15 16:56:34 -0400

    Update streamlit_app.py
    Inserted try except

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,20 +27,30 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please Select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # formats json data
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    # Shows formatted json data
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
+  
 streamlit.write('The user entered ', fruit_choice)
 
 
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 
-# formats json data
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# Shows formatted json data
-streamlit.dataframe(fruityvice_normalized)
+
+
 
 streamlit.stop()
 
